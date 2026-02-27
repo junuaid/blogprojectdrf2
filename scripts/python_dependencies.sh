9951c16f4d686fc68c331345904370206fe51116
junuaid <jjunu6541@gmail.com>
2026-02-27 23:00:14 +0530

    Update python_dependencies.sh for deployment setup

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
-cd /home/ubuntu/blogprojectdrf2
+set -e
+
+echo "Moving to deployment directory..."
+
+# Go to project root (CodeDeploy archive location)
+cd /opt/codedeploy-agent/deployment-root///deployment-archive
+
+echo "Current directory:"
+pwd
+ls
+
+echo "Creating virtual environment..."
 
 python3 -m venv venv
+
+echo "Activating virtual environment..."
 source venv/bin/activate
 
+echo "Installing dependencies..."
 pip install -r requirements.txt
