465792a058b92e3b9f91efed0eaa501aa8e896f0
junuaid <jjunu6541@gmail.com>
2026-02-28 00:45:04 +0530

    Improve echo messages in python_dependencies.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,19 +3,25 @@ set -e
 
 APP_DIR="/home/ubuntu/app"
 
+echo "Updating packages..."
+apt-get update -y
+
+echo "Installing python venv..."
+apt-get install -y python3-venv python3-pip
+
 echo "Moving to app directory..."
 cd $APP_DIR
 
 echo "Creating virtual environment..."
 python3 -m venv venv
 
-echo "Activating venv..."
+echo "Activating virtual environment..."
 source venv/bin/activate
 
 echo "Upgrading pip..."
 pip install --upgrade pip
 
-echo "Installing dependencies..."
+echo "Installing requirements..."
 pip install -r requirements.txt
 
-echo "Dependencies installed successfully!"
+echo "DONE ✅"
